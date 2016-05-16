26181d48126cca0929fdfc4b5abce1c92f72cd97
simonxiao <sixiao@microsoft.com>
2016-05-16 11:16:16 -0700

    change the folder structure of middleware benchmarking

--- middleware/mongodb-ycsb/run_mongodb-ycsb.sh
+++ middleware/mongodb-ycsb/run_mongodb-ycsb.sh
@@ -14,8 +14,8 @@ do
 	ssh root@${server} "sar -n DEV 1 900   2>&1 > /root/benchmark/mongodb/logs/$threads/$threads.sar.netio.log " & 
 	ssh root@${server} "iostat -x -d 1 900 2>&1 > /root/benchmark/mongodb/logs/$threads/$threads.iostat.diskio.log " &
 	ssh root@${server} "vmstat 1 900 2>&1 > /root/benchmark/mongodb/logs/$threads/$threads.vmstat.memory.cpu.log " & 
-	#./ycsb-0.5.0/bin/ycsb load mongodb-async -s -P workloadAzure -p mongodb.url=mongodb://192.168.4.88:27017/ycsb?w=0 -threads $threads > ./logs/$threads/$threads.ycsb.load.log
-	./ycsb-0.5.0/bin/ycsb  run  mongodb-async -s -P workloadAzure -p mongodb.url=mongodb://${server}:27017/ycsb?w=0 -threads $threads > ./$threads.ycsb.run.log
+	#./ycsb-0.5.0/bin/ycsb load mongodb-async -s -P LisWorkloadA -p mongodb.url=mongodb://192.168.4.88:27017/ycsb?w=0 -threads $threads > ./logs/$threads/$threads.ycsb.load.log
+	./ycsb-0.5.0/bin/ycsb  run  mongodb-async -s -P LisWorkloadA -p mongodb.url=mongodb://${server}:27017/ycsb?w=0 -threads $threads > ./$threads.ycsb.run.log
 	ssh root@${server} pkill -f sar
 	ssh root@${server} pkill -f iostat
 	ssh root@${server} pkill -f vmstat
