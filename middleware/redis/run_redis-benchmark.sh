65d618542ab9c7af923675309bc28ac5514299ff
simonxiao <sixiao@microsoft.com>
2016-06-01 11:22:26 -0700

    formatting redis test scripts

--- middleware/redis/run_redis-benchmark.sh
+++ middleware/redis/run_redis-benchmark.sh
@@ -19,39 +19,39 @@ do
         pipelines=${test_pipeline_collection[$t]}
         echo "NEXT TEST: $pipelines pipelines"
 
-		# prepare running redis-server
+        # prepare running redis-server
         ssh root@${server} mkdir -p                   $log_folder/$pipelines
         ssh root@${server} "sar -n DEV 1 900   2>&1 > $log_folder/$pipelines/$pipelines.sar.netio.log " &
         ssh root@${server} "iostat -x -d 1 900 2>&1 > $log_folder/$pipelines/$pipelines.iostat.diskio.log " &
         ssh root@${server} "vmstat 1 900       2>&1 > $log_folder/$pipelines/$pipelines.vmstat.memory.cpu.log " &
         ssh root@${server} $redis_path_server/redis-server > /dev/nul  &
 
-		# prepare running redis-benchmark
+        # prepare running redis-benchmark
         mkdir -p                   $log_folder/$pipelines
         sar -n DEV 1 900   2>&1  > $log_folder/$pipelines/$pipelines.sar.netio.log &
         iostat -x -d 1 900 2>&1  > $log_folder/$pipelines/$pipelines.iostat.diskio.log &
         vmstat 1 900       2>&1  > $log_folder/$pipelines/$pipelines.vmstat.memory.cpu.log &
 
-		#start running the redis-benchmark on client
+        #start running the redis-benchmark on client
         sleep 20
-		echo "-> start running"
+        echo "-> start running"
         $redis_path_client/redis-benchmark -h $server -c 1000 -P $pipelines -t $redis_test_suites -d 4000 -n 10000000 > $log_folder/$pipelines/$pipelines.redis.set.get.log
-		echo "-> done"
+        echo "-> done"
 		
-		#cleanup redis-server
+        #cleanup redis-server
         ssh root@${server} pkill -f sar
         ssh root@${server} pkill -f iostat
         ssh root@${server} pkill -f vmstat
         ssh root@${server} pkill -f redis-server > /dev/nul
 
-		#cleanup redis-benchmark
+        #cleanup redis-benchmark
         pkill -f sar
         pkill -f iostat
         pkill -f vmstat
         pkill -f redis-benchmark
 
         echo "sleep 60 seconds for next test"
-		echo ""
+        echo ""
 		
         sleep 60
         t=$(($t + 1))
