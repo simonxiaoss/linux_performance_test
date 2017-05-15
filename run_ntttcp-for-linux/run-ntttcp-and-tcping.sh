f7d1484a9d77922220fc4cacc15037136e6ef605
simonxiao <sixiao@microsoft.com>
2017-05-15 12:29:45 -0700

    Modify ntttcp-for-linux test with C10K TCP connections

--- run_ntttcp-for-linux/run-ntttcp-and-tcping.sh
+++ run_ntttcp-for-linux/run-ntttcp-and-tcping.sh
@@ -3,19 +3,19 @@
 log_folder=$1
 server_ip=$2
 server_username=$3
-test_run_duration=$4
-test_threads_collection=(1 2 4 8 16 32 64 128 256 512 1024 2048 3072 6144 10240)
+test_run_duration=60
+test_threads_collection=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 6144 8192 10240)
 max_server_threads=64
-eth_name=eth0
+eth_name=bond0
 
 if [[ $log_folder == ""  ]]
 then
-	log_folder=logs
+	log_folder="nettestlogs-$(date  +'%Y%m%d-%H%M')"
 fi
 
 if [[ $server_ip == ""  ]]
 then
-	server_ip=192.168.4.169
+	server_ip=192.168.4.113
 fi
 
 
@@ -41,6 +41,8 @@ if [ "$(which lagscope)" == "" ]; then
 fi
 
 eth_log="./$log_folder/eth_report.log"
+apt -y install bc sysstat dstat htop nload
+ssh $server_username@$server_ip "apt -y install bc sysstat dstat htop nload"
 
 function get_tx_bytes(){
 	# RX bytes:66132495566 (66.1 GB)  TX bytes:3067606320236 (3.0 TB)
@@ -66,18 +68,19 @@ function get_tx_pkts(){
 	echo $Tx_pkts 
 }
 
+ulimit -n 204800
 mkdir $log_folder
+ssh $server_username@$server_ip "ulimit -n 20480"
+ssh $server_username@$server_ip "mkdir $log_folder"
+
 rm -rf $eth_log 
 echo "#test_connections    throughput_gbps    average_packet_size" > $eth_log 
 
-ssh $server_username@$server_ip "pkill -f ntttcp"
 previous_tx_bytes=$(get_tx_bytes)
 previous_tx_pkts=$(get_tx_pkts)
 i=0
 while [ "x${test_threads_collection[$i]}" != "x" ]
 do
-	echo "Sleeping 10 seconds..."
-	sleep 10
 	current_test_threads=${test_threads_collection[$i]}
 	if [ $current_test_threads -lt $max_server_threads ]
 	then
@@ -93,14 +96,23 @@ do
 	echo "======================================"
 	
 	ssh $server_username@$server_ip "pkill -f ntttcp"
-	ssh $server_username@$server_ip "ulimit -n 102400  && ntttcp -r -e -P $num_threads_P -t ${test_run_duration}" &
+	ssh $server_username@$server_ip "ntttcp -P $num_threads_P -t ${test_run_duration} -e > ./$log_folder/ntttcp-receiver-p${num_threads_P}X${num_threads_n}.log" &
+
 	ssh $server_username@$server_ip "pkill -f lagscope"
 	ssh $server_username@$server_ip "lagscope -r" &
 	
+	ssh $server_username@$server_ip "pkill -f dstat"
+	ssh $server_username@$server_ip "dstat -dam > ./$log_folder/dstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
+	
+	ssh $server_username@$server_ip "pkill -f mpstat"
+	ssh $server_username@$server_ip "mpstat -P ALL 1 ${test_run_duration} > ./$log_folder/mpstat-receiver-p${num_threads_P}X${num_threads_n}.log" &
+	
 	sleep 2
+	sar -n DEV 1 ${test_run_duration} > "./$log_folder/sar-sender-p${num_threads_P}X${num_threads_n}.log" &
+	dstat -dam > "./$log_folder/dstat-sender-p${num_threads_P}X${num_threads_n}.log" &
+	mpstat -P ALL 1 ${test_run_duration} > "./$log_folder/mpstat-sender-p${num_threads_P}X${num_threads_n}.log" &
 	lagscope -s$server_ip -t ${test_run_duration} -V > "./$log_folder/lagscope-ntttcp-p${num_threads_P}X${num_threads_n}.log" &
-	ulimit -n 102400
-	ntttcp -s${server_ip} -P $num_threads_P -n $num_threads_n -t ${test_run_duration}  > "./$log_folder/ntttcp-p${num_threads_P}X${num_threads_n}.log"
+	ntttcp -s${server_ip} -P $num_threads_P -n $num_threads_n -t ${test_run_duration}  > "./$log_folder/ntttcp-sender-p${num_threads_P}X${num_threads_n}.log"
 
 	current_tx_bytes=$(get_tx_bytes)
 	current_tx_pkts=$(get_tx_pkts)
@@ -119,8 +131,9 @@ do
 	i=$(($i + 1))
 	sleep 5
 done
-	
-	 
+
+pkill -f dstat
+ssh $server_username@$server_ip "pkill -f dstat"
 ssh $server_username@$server_ip "pkill -f ntttcp"
 ssh $server_username@$server_ip "pkill -f lagscope"
 
